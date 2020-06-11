be63473defb28fd42c7b5df57c954b732fb2cf5e
Yury Bialkevich <darkerjura@gmail.com>
2020-06-11 14:52:24 +0300

    reimplemented method of calculating logic

--- src/App.java
+++ src/App.java
@@ -6,19 +6,13 @@ import java.io.InputStreamReader;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
-import java.util.Arrays;
 import java.util.Formatter;
-import java.util.HashSet;
 
 public class App {
     private static final String HMAC_SHA256 = "HmacSHA256";
 
     public static void main(String[] args) throws IOException {
-        HashSet<String> countryHashSet = new HashSet<>(Arrays.asList(args));
-        if (countryHashSet.size() < args.length){
-            System.out.println("Use different value");
-            System.exit(0);
-        } else if (args.length == 3 || args.length == 5) {
+        if (args.length % 2 != 0) {
             BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
             SecureRandom random = new SecureRandom();
 
@@ -26,7 +20,7 @@ public class App {
             int aiStep =  random.nextInt(args.length);
             String hmac = getHmac(key, args[aiStep]);
 
-            System.out.println(hmac);
+            System.out.println("HMAC: " + hmac);
             for (int i = 0; i < args.length; i++) {
                 System.out.println(i + 1 + " - " + args[i]);
             }
@@ -38,10 +32,10 @@ public class App {
 
             System.out.print("Computer move: ");
             System.out.println(args[aiStep]);
-            gameProcess(aiStep, myStep);
+            gameProcess(aiStep, myStep, args);
             System.out.println("HMAC key: " + key);
         } else {
-            System.out.println("Incorrect count value");
+            System.out.println("Incorrect count variable");
         }
     }
 
@@ -51,36 +45,30 @@ public class App {
             System.exit(0);
         }else if (myStep < 0 || myStep > args.length) {
             System.out.println("Please enter variable between 1 and " + args.length);
-            myStep = getMyStep(args, reader);
+            getMyStep(args, reader);
         }
         return myStep - 1;
     }
 
-    private static void gameProcess(int aiStep, int myStep) {
+    private static void gameProcess(int aiStep, int myStep, String[] args) {
+        String answer = "";
         if (myStep == aiStep) {
             System.out.println("Draw");
-        } else if (myStep == 0 && (aiStep == 1 || aiStep == 2)) {
-            System.out.println("You lose");
-        } else if (myStep == 1 && (aiStep == 2 || aiStep == 3)) {
-            System.out.println("You lose");
-        } else if (myStep == 2 && (aiStep == 3 || aiStep == 4)) {
-            System.out.println("You lose");
-        } else if (myStep == 3 && (aiStep == 4 || aiStep == 1)) {
-            System.out.println("You lose");
-        } else if (myStep == 4 && (aiStep == 1 || aiStep == 2)) {
-            System.out.println("You lose");
+        } else if (myStep > aiStep) {
+            answer = (myStep - aiStep > args.length / 2) ? "You lose" : "You win";
         } else {
-            System.out.println("You win");
+            answer = (aiStep - myStep > args.length / 2) ? "You win" : "You lose";
         }
+        System.out.println(answer);
     }
 
     private static String getRandomHexString() {
         SecureRandom random = new SecureRandom();
         StringBuilder sb = new StringBuilder();
-        while (sb.length() < 32) {
+        while (sb.length() < 64) {
             sb.append(Integer.toHexString(random.nextInt()));
         }
-        return sb.toString().substring(0, 32);
+        return sb.toString().substring(0, 64);
     }
 
     private static String toHexString(byte[] bytes) {
